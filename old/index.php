c7bacfd904c4b60907953d8816d6a48da87274b8
Twag2 <twag2team@gmail.com>
2014-08-12 20:59:28 +0700

    Road to MVC

---
+++ old/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Twag2 by Malakhov&Prokopyev</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/navbar.css" rel="stylesheet">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="navbar navbar-inverse navbar-static-top">
+    <style>
+        .body{padding-top:70px}
+    </style>
+    <div class="container">
+        <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Twag2</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="">
+            <div class="col-xs-2">
+                <a data-toggle="dropdown"><img src="http://placehold.it/120x120" class="center-block img-circle img-responsive"><span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="#">Change</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-6">
+            <h4>Your name</h4>
+            <h5>I'm fine</h5>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="row">
+                <img src="http://maps.googleapis.com/maps/api/staticmap?center=Novosibirsk&amp;zoom=12&amp;size=640x100&amp;scale=2&amp;sensor=true"
+                     class="center-block img-responsive">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <div class="navbar-center">
+            <a class="navbar-btn btn btn-default">Profile</a>
+            <a class="navbar-btn btn btn-default">Feed</a>
+            <a class="btn-lg btn btn-success">Action</a>
+            <a class="navbar-btn btn btn-default">News</a>
+            <a class="navbar-btn btn btn-default">Trend</a>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
---
+++ old/welcome.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome - Twag2</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="welcome.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <h1 class="form-welcome-heading">Здравствуйте!</h1>
+    <h2 class="form-welcome-heading">Добро пожаловать в Twag2!</h2>
+</div>
+<div id="footer">
+    <div class="container">
+        <button type="button" onClick="location.href='signup.php'" class="btn btn-lg navbar-btn btn-primary">Регистрация</button>
+        <button type="button" class="btn btn-lg navbar-btn btn-primary">Войти</button>
+    </div>
+</div>
+</body>
