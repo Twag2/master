091c2fe31a733648d4a7b991301a7d42b4bd16c3
Twag2 <twag2team@gmail.com>
2014-08-14 21:35:26 +0700

    new

--- application/views/signup_view.php
+++ application/views/signup_view.php
@@ -4,10 +4,10 @@
         <h2 class="form-signup-heading">Регистрация</h2>
         <div class="form-group"><input type="text" class="form-control" placeholder="Введите никнейм"/></div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Введите пароль"/>
-            <input type="password" class="form-control" placeholder="Введите пароль повторно"/>
+            <input type="text" class="form-control" placeholder="Введите пароль"/>
+            <input type="text" class="form-control" placeholder="Введите пароль повторно"/>
         </div>
-        <div class="form-group"><input type="email" class="form-control" placeholder="Введите адрес электронной почты"/></div>
+        <div class="form-group"><input type="text" class="form-control" placeholder="Введите адрес электронной почты"/></div>
         <div class="form-group"><input type="text" class="form-control" placeholder="Введите имя"/></div>
         <label class="checkbox">
             <input type="checkbox" value="remember-me"> Запомнить меня
