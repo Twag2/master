1c458d3ec71da862cd4e06eaee0aefe5aa783497
Twag2 <twag2team@gmail.com>
2014-08-14 21:45:01 +0700

    new

--- application/views/signup_view.php
+++ application/views/signup_view.php
@@ -2,19 +2,25 @@
 <div class="container">
     <form class="form-signup" action="" method="POST">
         <h2 class="form-signup-heading">Регистрация</h2>
-        <div class="form-group"><input type="text" class="form-control" placeholder="Введите никнейм"/></div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Введите никнейм"/>
+        </div>
         <div class="form-group">
             <input type="password" class="form-control" placeholder="Введите пароль"/>
         </div>
         <div class="form-group">
             <input type="password" class="form-control" placeholder="Введите пароль повторно"/>
         </div>
-        <div class="form-group"><input type="email" class="form-control" placeholder="Введите адрес электронной почты"/></div>
-        <div class="form-group"><input type="text" class="form-control" placeholder="Введите имя"/></div>
         <div class="form-group">
-        <label class="checkbox">
-            <input type="checkbox" value="remember-me"> Запомнить меня
-        </label>
+            <input type="email" class="form-control" placeholder="Введите адрес электронной почты"/>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Введите имя"/>
+        </div>
+        <div class="form-group">
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Запомнить меня
+            </label>
         </div>
         <button type="submit" class="btn btn-lg btn-primary btn-block">Принять</button>
 </form>
