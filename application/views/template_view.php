67554ca3e399c45e7922a3c22d402d69aae937fe
Twag2 <twag2team@gmail.com>
2014-08-15 21:59:31 +0700

    fixed scale

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -6,6 +6,7 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <?php include 'application/views/'.$content_view; ?>
