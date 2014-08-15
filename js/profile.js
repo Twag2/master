4f3b54946fff3e12c49543134cda5e652e522260
Twag2 <twag2team@gmail.com>
2014-08-16 04:34:58 +0700

    index2.php is going to be the best, but NOT 2DAY

--- js/profile.js
+++ js/profile.js
@@ -26,9 +26,15 @@ $(document).ready(function()
     $("#status").click(function()
     {
        $("#status").replaceWith(
-           '<div class="form-group">' +
-                '<input id="status" class="form-control" type="text" value="Status">' +
-                '<button class="btn btn-primary btn-sm pull-right" id="status_change" type="submit">Change</button>' +
+           '<div class="thumbnail">' +
+               '<form class="form-horizontal">' +
+                    '<div class="input-group">' +
+                        '<input class="form-control" type="text" value="Status">' +
+                        '<span class="input-group-btn">' +
+                        '<button class="btn btn-primary" id="status_change" type="submit">Change</button>' +
+                        '</span>' +
+                    '</div>' +
+               '</form>' +
            '</div>'
        );
        return false;
