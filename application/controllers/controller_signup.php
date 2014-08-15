1e62494efd3d501cdfa46b3a92ace789bbb548da
Twag2 <twag2team@gmail.com>
2014-08-16 03:10:05 +0700

    ##

--- application/controllers/controller_signup.php
+++ application/controllers/controller_signup.php
@@ -4,10 +4,67 @@ class Controller_Signup extends Controller
 {
     function action_index()
     {
-        $data = null;
         if($_SERVER['REQUEST_METHOD'] === 'POST')
         {
-            $data = $_POST;
+            if (isset($_POST['nick']))
+            {
+                $nick = $_POST['nick'];
+                $nick = stripslashes($nick);
+                $nick = htmlspecialchars($nick);
+                $nick = trim($nick);
+                if ($nick == '')
+                {
+                    unset($nick);
+                }
+            }
+            if (isset($_POST['password']) and isset($_POST['password_confirm']) and $_POST['password'] == $_POST['password_confirm'])
+            {
+                $password = $_POST['password'];
+                $password = stripslashes($password);
+                $password = htmlspecialchars($password);
+                $password = trim($password);
+                if ($password =='')
+                {
+                    unset($password);
+                }
+            }
+            if (isset($_POST['email']) and filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+            {
+                $email = $_POST['email'];
+                $email = stripslashes($email);
+                $email = htmlspecialchars($email);
+                $email = trim($email);
+                if ($email =='')
+                {
+                    unset($email);
+                }
+            }
+            if (isset($_POST['name']))
+            {
+                $name = $_POST['name'];
+                $name = stripslashes($name);
+                $name = htmlspecialchars($name);
+                $name = trim($name);
+                if ($name == '')
+                {
+                    unset($name);
+                }
+            }
+            if (empty($nick) or empty($password) or empty($email) or empty($name))
+            {
+                $this->view->generate('signup_view.php', 'template_view.php', 'Some error with data');
+            }
+            else
+            {
+                $data = array
+                (
+                    'nick' => $nick,
+                    'password' => $password,
+                    'email' => $email,
+                    'name' => $name
+                );
+                $this->model = new Model_Signup();
+            }
         }
         $this->view->generate('signup_view.php', 'template_view.php', $data);
     }
--- application/models/model_signup.php
+++ application/models/model_signup.php
@@ -2,14 +2,21 @@
 
 class Model_Signup extends Model
 {
+    private $db;
+    function __construct()
+    {
+        $this->db = mysql_connect("localhost", "twag228", "p3g5vbn7");
+    }
     function set_data($data)
     {
-        $db = mysql_connect("localhost", "twag228", "p3g5vbn7");
-        mysql_select_db("twag2240", $db);
+        mysql_select_db("twag2240", $this->db);
+        return mysql_query ("INSERT INTO users (nick,password,email,name) VALUES('{$data['nick']}','{$data['password']}','{$data['email']}','{$data['name']}')", $this->db);
+    }
 
-        {
-            $nick = $data['nick'];
-        }
-
+    function check_data($value)
+    {
+        mysql_select_db("twag2240", $this->db);
+        $result = mysql_query("SELECT id FROM users WHERE nick='$value'",$this->db);
+        return empty(mysql_fetch_array($result));
     }
 }
