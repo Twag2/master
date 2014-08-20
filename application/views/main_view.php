01d6cb1ecd3e08679801222973a7ca36d10b9af9
Twag2 <twag2team@gmail.com>
2014-08-20 17:51:49 +0700

    123

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,7 +3,7 @@
     <h1 class="form-welcome-heading">Здравствуйте!</h1>
     <h2 class="form-welcome-heading">Добро пожаловать в Twag2!</h2>
 </div>
-<div id="footer">
+<div id="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
         <button type="button" onClick="location.href='signup'" class="btn btn-lg navbar-btn btn-primary">Регистрация</button>
         <button type="button" class="btn btn-lg navbar-btn btn-primary">Войти</button>
